319fc8c3ecc3162d62327a2628522110d78c0c48
Boris R <rickrolled5997@gmail.com>
2019-07-01 21:58:41 +0300

    improved test function name

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.product_page import ProductPage
 
 
-def test_add_product_to_basket(browser):
+def test_guest_can_add_product_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     prod_page = ProductPage(browser, link)
     prod_page.open()
