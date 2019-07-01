c6b01c2271af7624e1f3984e022e40379be86dc2
Boris R <rickrolled5997@gmail.com>
2019-07-01 22:06:45 +0300

    improved assert expression on product page PO

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,7 +10,7 @@ class ProductPage(BasePage):
     def ensure_message_about_adding_is_present(self):
         product_added_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_HAS_BEEN_ADDED_ALERT)
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        assert product_name.text in product_added_alert.text, \
+        assert product_name.text == product_added_alert.text, \
             f'expected "{product_name.text}" product name, got "{product_added_alert.text}"'
 
     def ensure_product_price_is_equal_to_basket_total(self):
